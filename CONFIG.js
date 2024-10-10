f4dd4d2ad51ca5799a8ac415bed1dc6a1ead35e3
luyenno1 <114340901+luyenno1@users.noreply.github.com>
2024-10-10 21:50:15 +0700

    Update CONFIG.js
    Signed-off-by: luyenno1 <114340901+luyenno1@users.noreply.github.com>

--- CONFIG.js
+++ CONFIG.js
@@ -1,6 +1,7 @@
 const CONFIGDATA = {
   titleHeader: " </br> Làm người yêu tớ nhé!",
- 
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
   buttonYes: "Tớ đồng ý",
   buttonNo: "Không đồng ý",
   titleModar: "Cảm ơn cậu.",
